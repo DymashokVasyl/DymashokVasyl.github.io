bc5c7eaf02e62173e939bd7b4f23e38ed3649e29
DymashokVasyl <49263565+DymashokVasyl@users.noreply.github.com>
2020-05-31 15:06:34 +0300

    Add files via upload

---
+++ Landing-IT/js/main.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function () {
+
+    const mobMenu = document.querySelector('.mob-menu');
+    const navList = document.querySelector('.nav-list');
+
+    mobMenu.addEventListener('click', () => {
+        mobMenu.classList.toggle('active');
+        navList.classList.toggle('open');
+    });
+
+    function scroll(count, el, timeout, step) {
+        let i = 1;
+        (function () {
+            if (i <= count) {
+                setTimeout(arguments.callee, timeout);
+                document.querySelector('.' + el).innerHTML = i;
+                i = i + step;
+            } else {
+                document.querySelector('.' + el).innerHTML = count;
+
+            }
+        })();
+    }
+
+            scroll(225, 'count-1', 10, 2);
+            scroll(345, 'count-2', 10, 2);
+            scroll(365, 'count-3', 10, 2);
+            scroll(385, 'count-4', 10, 2);
+            scroll(385, 'count-5', 10, 2);
+
+    
+
+
+})
